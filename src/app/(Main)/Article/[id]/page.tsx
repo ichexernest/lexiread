8def965d1a37b852a4a21bca702315d8219d7cf2
Ichexernest <ch82831223@gmail.com>
2025-05-03 15:29:36 +0800

    can save in article

--- src/app/(Main)/Article/[id]/page.tsx
+++ src/app/(Main)/Article/[id]/page.tsx
@@ -2,13 +2,20 @@ import { fetchBbcArticleBySlug } from "@/utils/fetchBbcArticleBySlug"
 import ArticleViewer from "@/components/ArticleViewer"
 import { notFound } from "next/navigation"
 
+async function checkSaved(saveType: string, saveId: string): Promise<boolean> {
+  console.log("checkSaved", saveId, saveType);
+  await new Promise(res => setTimeout(res, 2000));
+  return Math.random() > 0.5;
+}
+
 
 export default async function ArticleContentPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params
   const article = await fetchBbcArticleBySlug(id)
+  const isSaved = await checkSaved("article", id);
 
   if (!article) return notFound()
 
-  return <ArticleViewer article={article} />
+  return <ArticleViewer article={article} isSaved={isSaved} />
 }
 
